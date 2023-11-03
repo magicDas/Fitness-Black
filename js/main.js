4f1bf514a5a2ea868ec36097dd8ff065d0ad3578
Peter Tor <40521286+magicDas@users.noreply.github.com>
2023-11-03 12:33:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,135 +1,27 @@
-/*  ---------------------------------------------------
-  Template Name: Activitar
-  Description:  Activitar Fitness HTML Template
-  Author: Colorlib
-  Author URI: https://colorlib.com
-  Version: 1.0
-  Created: Colorlib
----------------------------------------------------------  */
+document.addEventListener("DOMContentLoaded", function () {
+    var mobileMenuBtn = document.querySelector('.btn-mobile-menu');
+    var sideNav = document.querySelector('.sidenav');
+    var offCanvas = document.querySelector('.offcanvas');
+    var btnPlay = document.querySelector('.btn-play');
 
-'use strict';
-
-(function ($) {
-
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-
-        /*------------------
-            Gallery filter
-        --------------------*/
-        $('.gallery-controls ul li').on('click', function() {
-            $('.gallery-controls ul li').removeClass('active');
-            $(this).addClass('active');
-        });
-        if($('.gallery-filter').length > 0 ) {
-            var containerEl = document.querySelector('.gallery-filter');
-            var mixer = mixitup(containerEl);
-        }
-
-        $('.blog-gird').masonry({
-			itemSelector: '.grid-item',
-			columnWidth: '.grid-sizer',
-		});
-
+    mobileMenuBtn.addEventListener('click', function () {
+        sideNav.classList.toggle('open');
+        offCanvas.classList.toggle('open');
     });
 
-    /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
+    document.querySelector('.close').addEventListener('click', function () {
+        sideNav.classList.toggle('open');
+        offCanvas.classList.toggle('open');
     });
 
-    /*------------------
-		Navigation
-	--------------------*/
-    $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true
+    offCanvas.addEventListener('click', function () {
+        sideNav.classList.toggle('open');
+        offCanvas.classList.toggle('open');
     });
 
-    /*------------------
-		Menu Hover
-	--------------------*/
-    $(".header-section .nav-menu .mainmenu ul li").on('mousehover', function() {
-        $(this).addClass('active');
-    });
-    $(".header-section .nav-menu .mainmenu ul li").on('mouseleave', function() {
-        $('.header-section .nav-menu .mainmenu ul li').removeClass('active');
+    btnPlay.addEventListener('click', function () {
+        document.querySelector('.movie').classList.toggle('show');
+        btnPlay.classList.toggle('hidden');
     });
 
-    /*------------------
-        Carousel Slider
-    --------------------*/
-    $(".hero-items").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
-        dots: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        navText: ['<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-    });
-
-    /*------------------
-        Testimonial Slider
-    --------------------*/
-   $(".testimonial-slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: false,
-        items: 1,
-        dots: true,
-        navText: ['<i class="arrow_carrot-left"></i>', '<i class="arrow_carrot-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-    });
-
-    /*------------------
-        Magnific Popup
-    --------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
-
-    $('.image-popup').magnificPopup({
-        type: 'image'
-    });
-
-    /*------------------
-        Magnific Popup
-    --------------------*/
-    $('.show-result-select').niceSelect();
-
-    /*------------------
-       Timetable Filter
-    --------------------*/
-    $('.timetable-controls ul li').on('click', function() {
-        var tsfilter = $(this).data('tsfilter');
-        $('.timetable-controls ul li').removeClass('active');
-        $(this).addClass('active');
-        
-        if(tsfilter == 'all') {
-            $('.classtime-table').removeClass('filtering');
-            $('.ts-item').removeClass('show');
-        } else {
-            $('.classtime-table').addClass('filtering');
-        }
-        $('.ts-item').each(function(){
-            $(this).removeClass('show');
-            if($(this).data('tsmeta') == tsfilter) {
-                $(this).addClass('show');
-            }
-        });
-    });
-
-})(jQuery);
+});
